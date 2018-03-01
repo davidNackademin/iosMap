2f15a646f598e2a65a94106d219fb2b928d999f8
David <davidsvson@gmail.com>
2018-03-01 09:54:30 +0100

    Initial Commit

---
+++ thu1marsMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thu1marsMaps
+//
+//  Created by David Svensson on 2018-03-01.
+//  Copyright © 2018 David Svensson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
